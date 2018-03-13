86969fca1c3b6aa226dfa60a03f6e5cc56a87ceb
Chris van Zomeren <vanzomerenc@gmail.com>
2018-03-12 22:46:26 -0400

    Initialize all pins on startup

--- main.c
+++ main.c
@@ -9,6 +9,12 @@
 
 int main(void)
 {
+    for(int i = 0; i <= 10; i++)
+    {
+        MAP_GPIO_setAsOutputPin(i, PIN_ALL8);
+        MAP_GPIO_setOutputLowOnPin(i, PIN_ALL8);
+    }
+
     init_clocks();
     expect_frequency(CS_MCLK, 48000000);
     expect_frequency(CS_SMCLK, 24000000);
